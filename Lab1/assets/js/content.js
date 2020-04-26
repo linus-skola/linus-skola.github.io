599c1455e6de2ddad46b41feb7f7153cf0c78be0
Linus Örnborg <linus.skola@ornborg.se>
2020-04-26 19:37:05 +0200

    added onclick function to cart icon

--- Lab1/assets/js/content.js
+++ Lab1/assets/js/content.js
@@ -3,14 +3,14 @@ window.addEventListener('load', () => {
 
     cartcount = sessionStorage.getItem('cart');
 
-    var cart = document.getElementById('cart');
+    var cart = document.getElementById('cartcount');
     cart.textContent = cartcount;
 
     var adds = document.getElementsByClassName("addtocart-btn");
 
     $(adds).on("click", function () {
         cartcount++;
-        var cart = document.getElementById('cart');
+        var cart = document.getElementById('cartcount');
         cart.textContent = cartcount;
 
         sessionStorage.setItem('cart', cartcount);
@@ -18,7 +18,7 @@ window.addEventListener('load', () => {
 
     $('#cart').on("click", function () {
         cartcount = 0;
-        var cart = document.getElementById('cart');
+        var cart = document.getElementById('cartcount');
         cart.textContent = cartcount;
 
         sessionStorage.setItem('cart', cartcount);
